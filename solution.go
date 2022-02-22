d79946aa3767b4661f94042f2871f9516d3ea5d1
Oscar Pachon <odavidpachonguevara@atlassian.com>
2022-02-22 16:31:21 -0500

    Fix circle are calculation

--- solution.go
+++ solution.go
@@ -22,7 +22,7 @@ func CalcSquare(sideLen float64, sidesNum uint32) float64 {
 	case SidesSquare:
 		return sideLen * sideLen
 	case SidesCircle:
-		return math.Pi * sideLen
+		return math.Pi * math.Pow(sideLen, float64(2))
 	default:
 		return 0
 
